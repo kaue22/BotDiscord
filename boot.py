d6ff6b580df521138d308c329526ec257d3e9634
kaue <kauegouveia@gmail.com>
2021-12-04 09:06:25 -0300

    Bot data e hora
    o bot avisa a cada 100 segundos qual data e hora,se quiser mais tempo,altere o valor do @tasks.loop

--- boot.py
+++ boot.py
@@ -1,24 +1,29 @@
+import datetime
 import discord
-from discord.ext import commands
+from discord.ext import commands,tasks
 
 bot = commands.Bot("!")
 
 @bot.event
 async def on_ready():
     print(f"Estou pronto! Estou conectado como {bot.user}")
+    current_time.start()
 
 #Evento async para não retornar mensagens do bot
 @bot.event
 async def on_message(message):
+
     if message.author == bot.user:
         return
 
-    if "palavrão" in message.content:
-        await message.channel.snd(f"Por favor,{message.author.name},não ofenda os demais usuários!")
+        if "palavrão" in message.content:
+            await message.channel.send(
+                f"Por favor,{message.author.name},não ofenda os demais usuários!"
+            )
 
-        await message.delete()
+            await message.delete()
 
-    await  bot.process_commands(message)
+    await bot.process_commands(message)
 
 #Quando digitar o prefixo !oi executa a função send_hello
 @bot.command(name="oi")
@@ -28,6 +33,16 @@ async def send_hello(ctx):
     response = "Ola, " + name
 
     await ctx.send(response)
+#Atualiza data e hora no canal geral do servidor
+@tasks.loop(seconds=100)
+async def current_time():
+    now = datetime.datetime.now()
+
+    now = now.strftime("%d/%m/%Y às %H:%M:%S")
+
+    channel = bot.get_channel(915984953395785804) #id do canal geral
+
+    await channel.send("Data atual :" + now)
 
 
-bot.run("TOKE_AQUI")
+bot.run("TOKEN_AQUI") #TOKEN QUE VOCÊ PEGA NA PAGINA DE DESENVOLVEDOR DO DISCORD
